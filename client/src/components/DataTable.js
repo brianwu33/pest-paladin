89468d129f4ae047a71e743bcdec8a350dd79a73
Brian Wu <byhwu@uwaterloo.ca>
2024-07-26 01:00:02 -0400

    Add Delete in Detection Page and Update Server.js

--- client/src/components/DataTable.js
+++ client/src/components/DataTable.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import axios from 'axios';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
 const DataTable = ({ detections, handleViewClick }) => {
     if (!detections || detections.length === 0) {
         return <div>Loading...</div>;
     }
+
+    const handleDeleteClick = (detection) => {
+        confirmAlert({
+            title: 'Confirm to delete',
+            message: 'Are you sure you want to delete this detection?',
+            buttons: [
+                {
+                    label: 'Yes',
+                    onClick: () => deleteDetection(detection.instance_id)
+                },
+                {
+                    label: 'No'
+                }
+            ]
+        });
+    };
+
+    const deleteDetection = async (instance_id) => {
+        try {
+            await axios.delete(`http://localhost:4000/api/detections/${instance_id}`);
+            window.location.reload(); // Reload the page after deletion
+        } catch (error) {
+            console.error('Error deleting detection:', error);
+        }
+    };
 
     return (
         <div className="data-table-container">
@@ -14,7 +42,8 @@ const DataTable = ({ detections, handleViewClick }) => {
                         <th>Species</th>
                         <th>Camera ID</th>
                         <th>Duration</th>
-                        <th>Action</th>
+                        <th>Details</th>
+                        <th>Delete</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -28,6 +57,9 @@ const DataTable = ({ detections, handleViewClick }) => {
                             </td>
                             <td>
                                 <button onClick={() => handleViewClick(detection)}>View</button>
+                            </td>
+                            <td>
+                                <button onClick={() => handleDeleteClick(detection)}>Delete</button>
                             </td>
                         </tr>
                     ))}
