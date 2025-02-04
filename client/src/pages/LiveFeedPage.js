fbb263e7bde209cdd6dc606a7b199b9fa6d935d2
Andrew Liu <a242liu@uwaterloo.ca>
2025-02-04 16:50:54 -0500

    Working on Calender component

--- client/src/pages/LiveFeedPage.js
+++ client/src/pages/LiveFeedPage.js
@@ -3,49 +3,33 @@ import TopNavBar from '../components/TopNavBar';
 import Peer from "simple-peer"
 import io from "socket.io-client"
 
-const socket = io.connect("http://localhost:3001") // route to backend
+const socket = io.connect("http://localhost:5000") // route to backend
 
 const LiveFeedPage = () => {
-    const [callAccepted, setCallAccepted] = useState(false);
-    const userVideo = useRef();
-    const connectionRef = useRef();
+    const [isStreaming, setIsStreaming] = useState("false")
+    const videoRef = useRef(null);
 
-    useEffect(()=>{
-        socket.on("connection",()=>{
-            console.log("Connected to the server");
-        });
-
-        socket.on("offer", (data) =>{
-            const peer = new Peer({
-                initiator: false,
-                trickle: false,
-                stream: null
-            });
-
-            peer.on("signal", (signal) =>{
-                socket.emit("answer", {signal, target: data.from});
-            });
-
-            peer.on("stream", (stream) =>{
-                userVideo.current.srcObject = stream;
-                setCallAccepted(true);
-            });
-
-            peer.signal(data.signal);
-            connectionRef.current = peer;
-        });
-
-        return () =>{
-            socket.off("offer");
+    useEffect(() => {
+        const ws = new WebSocket("ws://localhost:5000"); // create WebSocket object, ws for local connectoin
+        ws.onmessage = (event) =>{
+            const blob = new Blob([event.data], {type: "image/jpeg"});
+            videoRef.current.src = URL.createObjectURL(blob);
         };
-    }, [])
-    
- 
+
+        return () => ws.close();
+    }, [isStreaming]) // run only when isStreaming Changes
+
+    const toggleStreaming = () =>{
+        isStreaming.setIsStreaming(!isStreaming);
+    }
+
     return (
         <div>
         <TopNavBar>
             <div className="video-container">
-            {callAccepted && <video ref={userVideo} autoPlay playsInline />}
+                {isStreaming ? (<button onClick={toggleStreaming}> Join the Livestream! </button>) :
+                    (videoRef.current  && <img ref={videoRef} alt="Live Stream"/>)
+                 }
             </div>
         </TopNavBar>
       </div>
